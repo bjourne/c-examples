f8ade05189bfc6fcef9e62291d32a79d209e810e
Björn Lindqvist <bjourne@gmail.com>
2016-11-10 03:29:32 +0100

    fixing the macros (missing brackets (bad))

--- libraries/datatypes/bits.h
+++ libraries/datatypes/bits.h
@@ -1,9 +1,12 @@
 #ifndef DATATYPES_BITS_H
 #define DATATYPES_BITS_H
 
-#define BF_BASIC_MASK(n)            ((1 << (n)) - 1)
-#define BF_MASK(start, len)         BF_BASIC_MASK(len) << (start)
-#define BF_GET(x, start, len)       (((x) >> (start)) & BF_BASIC_MASK(len))
-#define BF_SET(x, n, start, len)    ((x) &~ BF_MASK(start, len)) | ((n) << (start))
+#define BF_LIT_BITS(n)              ((1 << (n)) - 1)
+#define BF_MASK(start, len)         (BF_LIT_BITS(len) << (start))
+#define BF_GET(x, start, len)       (((x) >> (start)) & BF_LIT_BITS(len))
+#define BF_SET(x, n, start, len)    (((x) & ~BF_MASK(start, len)) | ((n) << (start)))
+
+// Bit munching
+#define ALIGN(a, b) ((a + (b - 1)) & ~(b - 1))
 
 #endif
--- tests/datatypes/bits.c
+++ tests/datatypes/bits.c
@@ -5,14 +5,20 @@
 
 void
 test_mask() {
-    printf("0x%x\n", BF_BASIC_MASK(16));
+    printf("0x%x\n", BF_LIT_BITS(16));
     printf("0x%x\n", BF_MASK(8, 24));
 
-    ptr m = BF_SET(m, 33, 8, 24);
+    ptr m = BF_SET(0, 33, 8, 24);
     assert(BF_GET(m, 8, 24) == 33);
     m = BF_SET(m, 100, 0, 8);
     assert(BF_GET(m, 8, 24) == 33);
     assert(BF_GET(m, 0, 8) == 100);
+
+    m = 0xffffff;
+    m = BF_SET(m, 2, 0, 4);
+    m = BF_SET(m, 4, 5, 3);
+    assert(BF_GET(m, 5, 3) == 4);
+    assert(BF_GET(m, 0, 4) == 2);
 }
 
 int
