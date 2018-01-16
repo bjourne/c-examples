c6500e51478acb1c91d07d4f44f4a7ed68a7f3b3
Björn Lindqvist <bjourne@gmail.com>
2018-01-16 19:42:39 +0100

    to please osx, the braced initializer lists need to be wrapped in paranthesis

--- libraries/isect/isect.c
+++ libraries/isect/isect.c
@@ -69,29 +69,29 @@ isect_bw9_pre(vec3 v0, vec3 v1, vec3 v2, isect_bw9_data *D) {
     if (fabsf(n.x) > fabsf(n.y) && fabsf(n.x) > fabsf(n.z)) {
         x1 = v1.y * v0.z - v1.z * v0.y;
         x2 = v2.y * v0.z - v2.z * v0.y;
-        memcpy(D, (float[9]){
+        memcpy(D, ((float[9]){
              e2.z / n.x, -e2.y / n.x,   x2 / n.x,
             -e1.z / n.x,  e1.y / n.x,  -x1 / n.x,
               n.y / n.x,   n.z / n.x, -num / n.x
-            }, 36);
+        }), 36);
         D->ci = 0;
     } else if (fabsf(n.y) > fabsf(n.z)) {
         x1 = v1.z * v0.x - v1.x * v0.z;
         x2 = v2.z * v0.x - v2.x * v0.z;
-        memcpy(D, (float[9]){
+        memcpy(D, ((float[9]){
             -e2.z / n.y,  e2.x / n.y,   x2 / n.y,
              e1.z / n.y, -e1.x / n.y,  -x1 / n.y,
               n.x / n.y,   n.z / n.y, -num / n.y
-        }, 36);
+        }), 36);
         D->ci = 1;
     } else {
         x1 = v1.x * v0.y - v1.y * v0.x;
         x2 = v2.x * v0.y - v2.y * v0.x;
-        memcpy(D, (float[9]){
+        memcpy(D, ((float[9]){
              e2.y / n.z, -e2.x / n.z,   x2 / n.z,
             -e1.y / n.z,  e1.x / n.z,  -x1 / n.z,
               n.x / n.z,   n.y / n.z, -num / n.z
-        }, 36);
+        }), 36);
         D->ci = -1;
     }
 }
@@ -106,27 +106,27 @@ isect_bw12_pre(vec3 v0, vec3 v1, vec3 v2, isect_bw12_data *D) {
     if (fabsf(n.x) > fabsf(n.y) && fabsf(n.x) > fabsf(n.z)) {
         x1 = v1.y * v0.z - v1.z * v0.y;
         x2 = v2.y * v0.z - v2.z * v0.y;
-        memcpy(D, (float[12]){
+        memcpy(D, ((float[12]){
             0,  e2.z / n.x, -e2.y / n.x,   x2 / n.x,
             0, -e1.z / n.x,  e1.y / n.x,  -x1 / n.x,
             1,   n.y / n.x,   n.z / n.x, -num / n.x
-        }, 48);
+        }), 48);
     } else if (fabsf(n.y) > fabsf(n.z)) {
         x1 = v1.z * v0.x - v1.x * v0.z;
         x2 = v2.z * v0.x - v2.x * v0.z;
-        memcpy(D, (float[12]){
+        memcpy(D, ((float[12]){
             -e2.z / n.y, 0,  e2.x / n.y,   x2 / n.y,
              e1.z / n.y, 0, -e1.x / n.y,  -x1 / n.y,
               n.x / n.y, 1,   n.z / n.y, -num / n.y
-        }, 48);
+        }), 48);
     } else {
         x1 = v1.x * v0.y - v1.y * v0.x;
         x2 = v2.x * v0.y - v2.y * v0.x;
-        memcpy(D, (float[12]){
+        memcpy(D, ((float[12]){
             e2.y / n.z, -e2.x / n.z, 0,   x2 / n.z,
            -e1.y / n.z,  e1.x / n.z, 0,  -x1 / n.z,
              n.x / n.z,   n.y / n.z, 1, -num / n.z
-        }, 48);
+        }), 48);
     }
 }
 
