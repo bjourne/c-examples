5943d7e0e81c4606ab3fc93952fd3556edae4948
Björn Lindqvist <bjourne@gmail.com>
2018-01-22 19:58:02 +0100

    windows compat: f on the end

--- tests/linalg/linalg.c
+++ tests/linalg/linalg.c
@@ -28,7 +28,7 @@ test_dot() {
 void
 test_normalize() {
     vec3 v1 = v3_normalize((vec3){30, -20, 8});
-    vec3 v2 = {0.81229556, -0.54153037, 0.21661215};
+    vec3 v2 = {0.81229556f, -0.54153037f, 0.21661215f};
     assert(v3_approx_eq(v1, v2));
 }
 
@@ -44,22 +44,22 @@ test_inverse() {
 
     mat4 m3 = {
         {
-            {0.707107, -0.331295, 0.624695, 0},
-            {0, 0.883452, 0.468521, 0},
-            {-0.707107, -0.331295, 0.624695, 0},
-            {-1.63871, -5.747777, -40.400412, 1}
+            {0.707107f, -0.331295f, 0.624695f, 0},
+            {0, 0.883452f, 0.468521f, 0},
+            {-0.707107f, -0.331295f, 0.624695f, 0},
+            {-1.63871f, -5.747777f, -40.400412f, 1}
         }
     };
     mat4 m4 = m4_inverse(m3);
     mat4 m5 = {
         {
-            { 0.70710695,  0,          -0.70710534,  0},
-            {-0.33129495,  0.88345194, -0.33129495,  0},
-            { 0.62469512,  0.46852198,  0.62469512,  0},
-            {24.49247551, 24.00636673,  22.17499161, 1}
+            { 0.70710695f,  0,          -0.70710534f,  0},
+            {-0.33129495f,  0.88345194f, -0.33129495f,  0},
+            { 0.62469512f,  0.46852198f,  0.62469512f,  0},
+            {24.49247551f, 24.00636673f,  22.17499161f, 1}
         }
     };
-    assert(m4_approx_eq2(m4, m5, 1e-5));
+    assert(m4_approx_eq2(m4, m5, 1e-5f));
 }
 
 void
@@ -74,7 +74,7 @@ test_mat_mul() {
     };
     vec3 v1 = {1, 2, 3};
     vec3 v2 = m4_mul_v3p(m1, v1);
-    assert(v3_approx_eq(v2, (vec3){0.70833331, 0.80555558, 0.90277779}));
+    assert(v3_approx_eq(v2, (vec3){0.70833331f, 0.80555558f, 0.90277779f}));
 }
 
 void
@@ -83,16 +83,27 @@ test_look_at() {
     vec3 center = {4.0f, 5.0f, 6.0f};
     vec3 up = {7.0f, 8.0f, 9.0f};
     mat4 view = m4_look_at(eye, center, up);
-    assert(approx_eq2(view.d[0][0], 0.408248276f, 1e-7));
-    assert(approx_eq2(view.d[3][3], 1.0, 1e-7));
+    assert(approx_eq2(view.d[0][0], 0.408248276f, 1e-7f));
+    assert(approx_eq2(view.d[3][3], 1.0f, 1e-7f));
 }
+
+void
+test_to_rad() {
+    assert(approx_eq(to_rad(70.0), 1.2217304706573486f));
+}
+
+void
+test_to_deg() {
+    assert(approx_eq(to_deg(to_rad(70.0)), 70.00000000));
+}
+
 
 void
 test_perspective() {
     mat4 persp = m4_perspective(to_rad(70.0f),
                                 500.0f/400.0f,
                                 0.1f, 1000.0f);
-    assert(approx_eq(persp.d[0][0], 1.142518401));
+    assert(approx_eq(persp.d[0][0], 1.142518401f));
     assert(persp.d[0][1] == 0.0f);
     assert(persp.d[0][2] == 0.0f);
     assert(persp.d[0][3] == 0.0f);
@@ -120,6 +131,8 @@ main(int argc, char *argv[]) {
     PRINT_RUN(test_inverse);
     PRINT_RUN(test_mat_mul);
     PRINT_RUN(test_look_at);
+    PRINT_RUN(test_to_rad);
+    PRINT_RUN(test_to_deg);
     PRINT_RUN(test_perspective);
     PRINT_RUN(test_get_plane);
     return 0;
