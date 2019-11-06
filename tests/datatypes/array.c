50942def8b6c9fa7640c16b473b551b82cfd6b7f
Björn Lindqvist <bjourne@gmail.com>
2019-11-06 06:13:32 +0100

    test keyed sorting

--- tests/datatypes/array.c
+++ tests/datatypes/array.c
@@ -22,9 +22,34 @@ test_array_shuffle() {
     }
 }
 
+typedef struct {
+    int *values;
+} sort_context;
+
+static int
+key_fun(void *ctx, const void *a) {
+    sort_context *s_ctx = (sort_context *)ctx;
+    return s_ctx->values[*(int *)a];
+}
+
+void
+test_sorting() {
+    int items[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int values[10] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+    sort_context ctx = { values };
+
+    array_qsort_with_key(items, 10, sizeof(int), &key_fun, &ctx);
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", items[i]);
+    }
+    printf("\n");
+}
+
 int
 main(int argc, char *argv[]) {
     rand_init(0);
     PRINT_RUN(test_array_shuffle);
+    PRINT_RUN(test_sorting);
     return 0;
 }
