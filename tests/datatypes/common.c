88f12a5ef1bcbf8de2064653403fd242c305b794
Björn Lindqvist <bjourne@gmail.com>
2023-05-29 14:08:56 +0200

    easy test

--- tests/datatypes/common.c
+++ tests/datatypes/common.c
@@ -4,7 +4,17 @@
 #include <string.h>
 #include "datatypes/common.h"
 
+void
+test_malloc_aligned() {
+    int dim = 1 << 15;
+    int n_bytes = dim * dim * 4;
+    char *data = malloc_aligned(64, 1 << 15);
+    assert(data);
+    free(data);
+}
+
 int
 main(int argc, char *argv[]) {
     rand_init(0);
+    PRINT_RUN(test_malloc_aligned);
 }
