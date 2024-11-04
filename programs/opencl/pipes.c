1f21fc9b008d6a863024a0ccab660c7b9623ae26
Björn Lindqvist <bjourne@gmail.com>
2024-11-04 21:00:12 +0100

    Make program compile... I still can't get OpenCL 3.0 pipes to work

--- programs/opencl/pipes.c
+++ programs/opencl/pipes.c
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 Björn A. Lindqvist <bjourne@gmail.com>
+// Copyright (C) 2022,2024 Björn A. Lindqvist <bjourne@gmail.com>
 //
 // Demonstrates how to use OpenCL pipes.
 #include <assert.h>
@@ -15,33 +15,38 @@ main(int argc, char *argv[]) {
 
     cl_uint n_devices;
     cl_device_id *devices;
-    ocl_get_devices(platforms[0], &n_devices, &devices);
+    ocl_get_devices(platforms[1], &n_devices, &devices);
 
     cl_device_id dev = devices[0];
     ocl_print_device_details(dev, 0);
 
     cl_context ctx = clCreateContext(NULL, 1, devices, NULL, NULL, &err);
-    ocl_check_err(err);
+    OCL_CHECK_ERR(err);
 
     cl_command_queue queue = clCreateCommandQueueWithProperties(
         ctx, dev, 0, &err);
-    ocl_check_err(err);
+    OCL_CHECK_ERR(err);
 
     // Load kernel
     cl_program program;
     cl_kernel kernel;
     printf("* Loading kernel\n");
-    assert(ocl_load_kernel(ctx, dev, "programs/opencl/pipes.cl",
-                           &program, &kernel));
+    char *kernel_name = "producer";
+    OCL_CHECK_ERR(ocl_load_kernels(
+                      ctx, dev,
+                      "programs/opencl/pipes.cl", "-cl-std=CL2.0 -Werror",
+                      1, &kernel_name,
+                      &program, &kernel
+                  ));
 
     // Release queue
-    clFlush(queue);
-    clFinish(queue);
-    clReleaseCommandQueue(queue);
+    OCL_CHECK_ERR(clFlush(queue));
+    OCL_CHECK_ERR(clFinish(queue));
+    OCL_CHECK_ERR(clReleaseCommandQueue(queue));
 
-    clReleaseKernel(kernel);
-    clReleaseProgram(program);
-    clReleaseContext(ctx);
+    OCL_CHECK_ERR(clReleaseKernel(kernel));
+    OCL_CHECK_ERR(clReleaseProgram(program));
+    OCL_CHECK_ERR(clReleaseContext(ctx));
     free(platforms);
     free(devices);
 
